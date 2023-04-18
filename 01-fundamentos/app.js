a8e960273c8f85fa759fc325ae33938227843f29
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-04-18 10:23:45 -0600

    Se subieron los archivos trabajados

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,19 @@
+
+console.log('Hola Mundo');
+let a = 10,
+     b = 20,
+     c = 'Hola ',
+     d = 'Spiderman',
+     x = a + b;
+
+const  saludo = c + d;
+
+console.log('%c Mis Variables', 'color:red; font-weigth: blod');
+console.log({a});
+console.log({b});
+console.log({c});
+
+c = 'Hola de Nuevo';
+
+console.table({a,b,c,d,x});
+
