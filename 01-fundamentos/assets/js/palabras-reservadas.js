8257df350f3b66280dbf172193c7ef8f570a190d
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-07-03 12:19:08 -0600

    Hola

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,4 @@
+
+
+let constante = 123;
+
--- 01-fundamentos/assets/js/primitivos.js
+++ 01-fundamentos/assets/js/primitivos.js
@@ -1,6 +1,45 @@
 
+//maneras para asignar strings
 let nombre = 'Emanuel Castaniaza';
 console.log(nombre);
 
 nombre = 'De Jesus Cifuentes',
 console.log(nombre);
+
+nombre = "Familia";
+nombre = 'Familia';
+
+//Esto nos dira que tipo es el dato
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+
+//Booleanos siempre ingresar en minusculas
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+
+//Numeros
+let edad = 33;
+console.log(typeof edad);
+
+//Conn decimales
+edad =  33.01
+console.log(typeof edad);
+
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1);
+
+console.log(symbol1 === symbol2);
