84213f8a6724f0c6abc37aed1d62bdee731f203a
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-07-31 16:31:34 -0600

    Se realizaron cambios

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -8,10 +8,15 @@ export const asyncAwaitComponent = async (element) => {
     const id1 = '5d86371f2343e37870b91ef1';
     const id2 = '5d86371f9f80b591f499df32';
 
-    const hero1 = await findHero(id1);
-    const hero2 = await findHero(id2);
+    try {
+        const hero1 = await findHero(id1);
+        const hero2 = await findHero(id2);
 
-    element.innerHTML = `${hero1.name} / ${hero2.name}`
+        element.innerHTML = `${hero1.name} / ${hero2.name}`
+
+    } catch (error) {
+        element.innerHTML = error;
+    }
 
 }
 
