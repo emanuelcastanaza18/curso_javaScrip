af4abd2b4edeeab34a0b3546228dbd7b8d8d97b9
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-08-21 17:58:06 -0600

    Se realizaron modificaciones

---
+++ 07-http-app/src/users/store/users-store.js
@@ -0,0 +1,34 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page";
+
+
+const state = {
+    currentPage: 0,
+    users: [],
+}
+
+const loadNextPage = async () => {
+    await loadUsersByPage(state.currentPage + 1);
+}
+
+const loadPreviousPage = async () => {
+    throw new Error('No implementado');
+}
+
+const onUserChanged = () => {
+    throw new Error('No implementado');
+}
+
+const reloadPage = async () => {
+    throw new Error('No implementado');
+}
+
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+
+    getuser: () => [...state.users],
+    getCurrentPage: () => state.currentPage,
+}
