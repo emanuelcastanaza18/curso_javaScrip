70829c5c6e2885e5cac05276de6d9313edefaeee
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-10-16 17:39:57 -0600

    Se realizaron avances en el codigo para post

---
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -0,0 +1,66 @@
+import modalHTML from './render-modal.html?raw';
+import './render-modal.css';
+
+let modal, form;
+
+
+export const showModal = () => {
+    modal?.classList.remove('hide-modal');
+}
+
+export const hideModal = () => {
+    modal?.classList.add('hide-modal');
+    form?.reset();
+
+}
+
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderModal = (element) => {
+
+    if (modal) return;
+
+    modal = document.createElement('div');
+    modal.innerHTML = modalHTML;
+    modal.className = 'modal-container hide-modal';
+    form = modal.querySelector('form');
+
+
+
+    modal.addEventListener('click', (event) => {
+        if (event.target.className === 'modal-container') {
+            hideModal();
+        }
+    });
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const userLike = {};
+
+        for (const [key, value] of formData) {
+            if (key === 'balance') {
+                userLike[key] = +value;
+                continue;
+            }
+
+            if (key === 'isActive') {
+                userLike[key] = (value === 'on') ? true : false;
+                continue;
+            }
+
+            userLike[key] = value;
+        }
+
+        hideModal();
+    });
+
+
+    element.append(modal);
+
+}
